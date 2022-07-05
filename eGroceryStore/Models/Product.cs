0cbfab16e85cc666f0db01d0de030186cf685963
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-07-05 17:03:49 +0300

    Add project files.

---
+++ eGroceryStore/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eGroceryStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProductBrand { get; set; }
+
+        public double Price { get; set; }
+
+        public int ProductStock { get; set; }
+
+        public string  Description { get; set; }
+
+        public string Ingredients { get; set; }
+    }
+}
