57f710e0235add0d66198e25277ea16d431bd4ca
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-07-05 17:35:23 +0300

    Add Category class

--- eGroceryStore/Models/Category.cs
+++ eGroceryStore/Models/Category.cs
@@ -2,18 +2,15 @@
 
 namespace eGroceryStore.Models
 {
-    public class Brand
+    public class Category
     {
         [Key]
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public string LogoUrl { get; set; }
-
-        public string Description { get; set; }
-
         //Relationships
         public List<Product> Products { get; set; }
+
     }
 }
