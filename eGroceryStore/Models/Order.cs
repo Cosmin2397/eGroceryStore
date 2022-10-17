a88e169dfc513b47176b1856e7d72dbe7ecfd1cf
Cosmin2397 <59839882+Cosmin2397@users.noreply.github.com>
2022-10-17 11:05:26 +0300

    Update code

--- eGroceryStore/Models/Order.cs
+++ eGroceryStore/Models/Order.cs
@@ -8,6 +8,7 @@ namespace eGroceryStore.Models
             public int Id { get; set; }
 
             public string Email { get; set; }
+
             public string UserId { get; set; }
 
             public List<OrderItem> OrderItems { get; set; }
--- eGroceryStore/Models/OrderItem.cs
+++ eGroceryStore/Models/OrderItem.cs
@@ -9,6 +9,7 @@ namespace eGroceryStore.Models
         public int Id { get; set; }
 
         public int Quantity { get; set; }
+
         public double Price { get; set; }
 
         public int ProductId { get; set; }
